dc372a2fd7a205135aa8ec424d0dd03710a3e9f7
MaxIlyuhin <iliuhin.maxim2016@yandex.ru>
2021-09-29 00:15:15 +0300

    CRUD without checking

--- Ilyuhin_lab/Ilyuhin_lab.cpp
+++ Ilyuhin_lab/Ilyuhin_lab.cpp
@@ -2,29 +2,130 @@
 //
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Pipe {
     int id;
     double length;
-
+    int diameter;
+    bool repair;
 };
-void print_length_of_pipe(Pipe& p)
-{
-    cout << "Pipe's length is" << " " << p.length;
-}
+struct CS {
+    int id;
+    string name;
+    int count_of_workshops;
+    int working_workshops;
+    double efficiency;
+};
 Pipe create_pipe() {
     Pipe p;
     p.id = 0;
-    cout << "Dear User, please, enter your pipe's length" << " ";
+    cout << "Please, enter the length of the pipe" << " ";
     cin >> p.length;
+    cout << "Please, enter the pipe diameter" << " ";
+    cin >> p.diameter;
+    cout << "In repair?" << " ";
+    cin >> p.repair;
     return p;
 }
-
+    CS create_cs() {
+    CS cs;
+    cs.id = 0;
+    cout << "Please, enter the name of the station" << " ";
+    cin >> cs.name;
+    cout << "Please, enter the number of workshops" << " ";
+    cin >> cs.count_of_workshops;
+    cout << "Please, enter the number of working workshops" << " ";
+    cin >> cs.working_workshops;
+    cout << "Please, enter the efficiency" << " ";
+    cin >> cs.efficiency;
+    return cs;
+}
+ void print_all(Pipe& p, CS& cs)
+    {
+        cout << "Pipe ID" << " " << 10 << endl;
+        cout << "Pipe length is" << " " << p.length << endl;
+        cout << "Pipe diameter is" << " " << p.diameter << endl;
+        cout << "In repair?" << " " << p.repair << endl;
+        cout << "CS ID" << " " << 20 << endl;
+        cout << "Name of CS" << " " << cs.name << endl;
+        cout << "Number of workshops" << " " << cs.count_of_workshops << endl;
+        cout << "Number of working workshops" << " " << cs.working_workshops << endl;
+        cout << "Efficiency" << " " << cs.efficiency << endl;
+   }
+void PrintMenu()
+{
+    cout << "1. Input pipe " << endl
+        << "2. Input CS " << endl
+        << "3. View all objects " << endl
+        << "4. Edit pipe " << endl
+        << "5. Edit CS " << endl
+        << "6. Save to file " << endl
+        << "7. Load to file " << endl
+        << "0. Exit " << endl
+        << "Select the action:" << " ";
+}
 int main()
 {
-    Pipe p = create_pipe();
-    print_length_of_pipe(p);
+    Pipe p;
+    CS cs;
+    //CS cs;
+    while (1)
+    {
+        PrintMenu();
+        int i = 0;
+        cin >> i;
+        switch (i)
+        {
+        case 1:
+        {
+            p = create_pipe();
+            break;
+        }
+        case 2:
+        {
+            cs = create_cs();
+            break;
+        }
+        case 3:
+        {
+            print_all (p, cs); //только все про трубу и все про КС
+            break;
+        }
+        case 4:
+        {
+            //EditPipe(p);
+            break;
+        }
+        case 5:
+        {
+            //EditCS(cs);
+            break;
+        }
+        case 6:
+        {
+            //SaveAll(p,cs);
+            break;
+        }
+        case 7:
+        {
+            // p = LoadPipe();
+            // cs = LoadPipe();
+            break;
+        }
+        case 0:
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "wrong action" << endl;
+        }
+        }
+    }
+    //Pipe p = create_pipe();
+    //print_length_of_pipe(p);
          
     return 0;
 }
