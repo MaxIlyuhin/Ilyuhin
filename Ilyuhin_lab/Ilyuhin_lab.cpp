2bd369ac3e7c415cf75fe2903ca9d18c6fcf81a2
MaxIlyuhin <iliuhin.maxim2016@yandex.ru>
2021-10-10 13:01:58 +0300

    Edit CS and Pipe, user mistakes

--- Ilyuhin_lab/Ilyuhin_lab.cpp
+++ Ilyuhin_lab/Ilyuhin_lab.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
 struct Pipe {
@@ -8,6 +10,7 @@ struct Pipe {
     int diameter;
     bool repair;
 };
+
 struct CS {
     int id;
     string name;
@@ -15,151 +18,256 @@ struct CS {
     int working_workshops;
     double efficiency;
 };
-Pipe create_pipe() {
-    Pipe p;
-    cout << "Please, enter id" << " ";
-    cin >> p.id;
-    cout << "Please, enter the length of the pipe" << " ";
-    cin >> p.length;
-    cout << "Please, enter the pipe diameter" << " ";
-    cin >> p.diameter;
-    cout << "In repair?" << " ";
-    cin >> p.repair;
+
+void PrintMenu()
+{
+    cout << "-------------------------" << endl
+        << "1. Input pipe " << endl
+        << "2. Input CS " << endl
+        << "3. View all objects " << endl
+        << "4. Edit pipe " << endl
+        << "5. Edit CS " << endl
+        << "6. Save to file " << endl
+        << "7. Load to file " << endl
+        << "0. Exit " << endl
+        << "Choose action:" << " ";
+}
+
+template <typename T>
+T GetCorrectNumber(T min, T max)
+{
+    T x;
+    while ((cin >> x).fail() || x < min || x > max || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Type number (" << min << "-" << max << "):" << " ";
+    } 
+        return x;
+}
+
+Pipe Edit_repair_pipe(Pipe& p)
+{
+    while (true)
+    {
+        cout << "in repair? [y, n]:" << " ";
+        char repair;
+        cin >> repair;
+        if (cin.peek() == '\n')
+        {
+            if (tolower(repair) == 'y')
+            {
+                p.repair = 1;
+                break;
+            }
+            else if (tolower(repair) == 'n')
+            {
+                p.repair = 0;
+                break;
+            }
+            else
+            {
+                cout << "Not correct input. Please, try again!" << endl;
+            }
+        }
+        else
+        {
+            cout << "Too many characters. Please, try again!" << endl;
+        }
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
     return p;
 }
-    CS create_cs() {
-    CS cs;
-    cout << "Please, enter id" << " ";
-    cin >> cs.id;
-    cout << "Please, enter the name of the station" << " ";
-    cin >> cs.name;
-    cout << "Please, enter the number of workshops" << " ";
-    cin >> cs.count_of_workshops;
-    cout << "Please, enter the number of working workshops" << " ";
-    cin >> cs.working_workshops;
-    cout << "Please, enter the efficiency" << " ";
-    cin >> cs.efficiency;
+
+Pipe create_pipe()
+{
+    Pipe p;
+    p.id = 1;
+    cout << "Please, enter the length of the pipe (m)" << " ";
+    p.length = GetCorrectNumber(1.0, 500000.0);
+    cout << "Please, enter the pipe diameter (mm)" << " ";
+    p.diameter = GetCorrectNumber(300, 1420);
+    p = Edit_repair_pipe(p);
+    return p;
+}
+
+CS Edit_shop_cs(CS& cs)
+{
+    cout << "Please, enter the number of shops in work" << " ";
+    cs.working_workshops = GetCorrectNumber(1, cs.count_of_workshops);
     return cs;
 }
-    void PrintMenu()
-    {
-        cout << "1. Input pipe " << endl
-            << "2. Input CS " << endl
-            << "3. View all objects " << endl
-            << "4. Edit pipe " << endl
-            << "5. Edit CS " << endl
-            << "6. Save to file " << endl
-            << "7. Load to file " << endl
-            << "0. Exit " << endl
-            << "Select the action:" << " ";
-    }
-    void print_pipe(const Pipe& p)
-    {
-        cout << "Pipe ID" << " " << p.id << endl;
-        cout << "Pipe length is" << " " << p.length << endl;
-        cout << "Pipe diameter is" << " " << p.diameter << endl;
-        cout << "In repair?" << " " << p.repair << endl;
-    }
-    void print_CS(const CS& cs)
-    {
-        cout << "CS ID" << " " << cs.id << endl;
-        cout << "Name of CS" << " " << cs.name << endl;
-        cout << "Number of workshops" << " " << cs.count_of_workshops << endl;
-        cout << "Number of working workshops" << " " << cs.working_workshops << endl;
-        cout << "Efficiency" << " " << cs.efficiency << endl;
-    }
- Pipe load_pipe() {
+
+CS create_cs() {
+    CS cs;
+    cs.id = 2;
+    cout << "Please, enter the name of the station" << " ";
+    cin.clear();
+    cin.ignore(10000, '\n');
+    getline(cin, cs.name);
+    cout << "Please, enter the number of shops" << " ";
+    cs.count_of_workshops = GetCorrectNumber(1, 10);
+    cs = Edit_shop_cs(cs);
+    cout << "Please, enter the efficiency" << " ";
+    cs.efficiency = GetCorrectNumber(0.0, 1.0);
+    return cs;
+}
+
+void print_pipe(const Pipe& p)
+ {
+    cout << "Pipe ID" << " " << p.id << endl;
+    cout << "Pipe length is" << " " << p.length << endl;
+    cout << "Pipe diameter is" << " " << p.diameter << endl;
+    cout << "status:" << " " << ((p.repair == 0) ? "in work" : "in repair") << endl;
+ }
+    
+void print_CS(const CS& cs)
+{
+    cout << "CS ID" << " " << cs.id << endl;
+    cout << "Name of CS" << " " << cs.name << endl;
+    cout << "Number of workshops" << " " << cs.count_of_workshops << endl;
+    cout << "Number of working workshops" << " " << cs.working_workshops << endl;
+    cout << "Efficiency" << " " << cs.efficiency << endl;
+}
+    
+Pipe load_pipe() {
      Pipe p;
      ifstream fin;
      fin.open("pipe.txt", ios::in);
-     fin >> p.id;
-     fin >> p.length;
-     fin >> p.diameter;
-     fin >> p.repair;
-     fin.close();
+     if (fin.is_open())
+     {
+         fin >> p.id;
+         fin >> p.length;
+         fin >> p.diameter;
+         fin >> p.repair;
+         fin.close();
+     }
      return p;
  }
- CS load_CS() {
+
+CS load_CS() {
      CS cs;
      ifstream fin;
      fin.open("CS.txt", ios::in);
-     fin >> cs.id;
-     fin >> cs.name;
-     fin >> cs.count_of_workshops;
-     fin >> cs.working_workshops;
-     fin >> cs.efficiency;
-     fin.close();
+     if (fin.is_open())
+     {
+         fin >> cs.id;
+         fin >> cs.name;
+         fin >> cs.count_of_workshops;
+         fin >> cs.working_workshops;
+         fin >> cs.efficiency;
+         fin.close();
+     }
      return cs;
  }
+
  void save_pipe(const Pipe& p)
  {
      ofstream fout;
      fout.open("pipe.txt", ios::out);
-     fout << p.id << endl
-         << p.length << endl
-         << p.diameter << endl
-         << p.repair << endl;
-     fout.close();
+     if (fout.is_open())
+     {
+         fout << p.id << endl
+             << p.length << endl
+             << p.diameter << endl
+             << p.repair << endl;
+         fout.close();
+     }
  }
+
  void save_CS(const CS& cs)
  {
      ofstream fout;
      fout.open("CS.txt", ios::out);
-     fout << cs.id << endl
-         << cs.name << endl
-         << cs.count_of_workshops << endl
-         << cs.working_workshops << endl
-         << cs.efficiency << endl;
+     if (fout.is_open())
+     {
+         fout << cs.id << endl
+             << cs.name << endl
+             << cs.count_of_workshops << endl
+             << cs.working_workshops << endl
+             << cs.efficiency << endl;
+     }
  }
+
 int main()
 {
     Pipe p;
     CS cs;
+    bool pipe_created = false;
+    bool cs_created = false;
     while (1)
     {
         PrintMenu();
-        int i = 0;
-        cin >> i;
-        switch (i)
+        switch (GetCorrectNumber(0, 7))
         {
         case 1:
         {
             p = create_pipe();
+            pipe_created = true;
             break;
         }
         case 2:
         {
             cs = create_cs();
+            cs_created = true;
             break;
         }
         case 3:
         {
-            print_pipe(p);
-            print_CS(cs);
-           // print_cs(cs);//только все про трубу и все про КС
+            if(pipe_created && cs_created)
+            {
+
+                print_pipe(p);
+                print_CS(cs);
+            }
+            else
+            {
+               cout << "Not all elements are created!" << endl;
+            }
             break;
         }
         case 4:
         {
-            //EditPipe(p);
+            if (pipe_created)
+            {
+                Edit_repair_pipe(p);
+            }
+            else
+            {
+                cout << "Pipe in not created!" << endl;
+            }
             break;
         }
         case 5:
         {
-            //EditCS(cs);
+            if (cs_created)
+            {
+                Edit_shop_cs(cs);
+            }
+            else
+            {
+                cout << "CS in not created!" << endl;
+            }
             break;
         }
         case 6:
         {
-            save_pipe(p);
-            save_CS(cs);
-            //SaveAll(p,cs);
+            if (pipe_created && cs_created)
+            {
+                save_pipe(p);
+                save_CS(cs);
+            }
+            else
+            {
+                cout << "not all elements are created" << endl;
+            }
             break;
         }
         case 7:
         {
-           // print_pipe(load_pipe());
-            print_pipe(load_pipe());
+           print_pipe(load_pipe());
+
             print_CS(load_CS());
             // p = LoadPipe();
             // cs = LoadPipe();
@@ -174,9 +282,6 @@ int main()
             cout << "wrong action" << endl;
         }
         }
-    }
-    //Pipe p = create_pipe();
-    //print_length_of_pipe(p);
-         
+    }         
     return 0;
 }
